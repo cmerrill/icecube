2f04b60e4a2772e31b8b02611ecf69d5f3be7bec
Chris Merrill <cmerrill@alum.mit.edu>
2013-09-08 21:45:36 -0700

    Yay! Finished everything but the actual beat detection.

--- firmware/beat_detect.c
+++ firmware/beat_detect.c
@@ -8,15 +8,20 @@
 #include "beat_detect.h"
 
 void beat_detect_init(void) {
-
+	ADMUX = _BV(MUX1) + _BV(MUX0);
+	ADCSRA = _BV(ADPS2) + _BV(ADPS0);
+	ADCSRB = 0;
+	DIDR0 = _BV(ADC3D);
 }
 
 void beat_detect_start_conversion(void) {
-
+	ADCSRA |= _BV(ADEN) + _BV(ADSC);
 }
 
 uint16_t beat_detect_get_last_conversion(void) {
-	return 0;
+	uint16_t out = ADCL;
+	out += ((uint16_t)ADCH) << 8;
+	return out;
 }
 
 bool beat_detected(uint16_t level) {
